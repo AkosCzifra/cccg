5946a30e1866abf15cfe51add1be24e100811002
Rudi Szabó <rudi.szabo@codecool.com>
2019-05-30 11:23:02 +0200

    main function; cards generated from list

--- static/js/CCCG.js
+++ static/js/CCCG.js
@@ -1,24 +1,28 @@
-function createCard(name, power, image, health, mana, position){
+function createCard(cardData) {
     let card = document.createElement("div");
     card.id = "card";
     card.setAttribute("draggable", "true");
-    card.addEventListener("dragstart", function () {dragstartHandler(event)}, false);
+    card.addEventListener("dragstart", function () {
+        dragstartHandler(event)
+    }, false);
     card.className = "playing-card";
     card.setAttribute("draggable", "true");
     let cardName = document.createElement("div");
-    cardName.textContent = name;
+    cardName.textContent = cardData.name;
     cardName.className = "card-name";
     let cardPower = document.createElement("div");
-    cardPower.textContent = power;
+    cardPower.textContent = cardData.power;
     cardPower.className = "power-number";
     let cardImage = document.createElement("img");
-    cardImage.src = `${image}`;
-    cardImage.ondragstart = function() { return false; };
+    cardImage.src = `${cardData.image}`;
+    cardImage.ondragstart = function () {
+        return false;
+    };
     let cardHealth = document.createElement("div");
-    cardHealth.textContent = health;
+    cardHealth.textContent = cardData.health;
     cardHealth.className = "health-number";
     let cardMana = document.createElement("div");
-    cardMana.textContent = mana;
+    cardMana.textContent = cardData.mana;
     cardMana.className = "mana-number";
 
     card.appendChild(cardPower);
@@ -27,53 +31,26 @@ function createCard(name, power, image, health, mana, position){
     card.appendChild(cardMana);
     card.appendChild(cardName);
 
-    card.dataset.power = power;
-    card.dataset.health = health;
-    card.dataset.mana = mana;
+    card.dataset.power = cardData.power;
+    card.dataset.health = cardData.health;
+    card.dataset.mana = cardData.mana;
 
 
-    document.querySelector(position).appendChild(card)
+    let cardSlots = document.querySelectorAll('.inHandCards > div');
+    let emptySlot = null;
+    for (let slot of cardSlots) {
+        if (slot.innerHTML.trim() === "") {
+            emptySlot = slot;
+            break;
+        }
+    }
+
+    if (emptySlot) {
+        emptySlot.appendChild(card);
+    }
 }
 
-createCard(
-
-    "Nemo",
-    2,
-    "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
-    6,
-    9,
-    "#first-card");
-
-createCard(
-
-    "Nemo",
-    2,
-    "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
-    6,
-    9,
-    "#second-card");
-
-createCard(
-
-    "Nemo",
-    2,
-    "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
-    6,
-    9,
-    "#third-card");
-
-createCard(
-
-    "Nemo",
-    2,
-    "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
-    6,
-    9,
-    "#fourth-card");
-
-let playerHealth = 100;
-
-function displayHealth(playerHealth){
+function displayHealth(playerHealth) {
     let health = document.querySelector("#player-health");
 
     let image = document.createElement("img");
@@ -83,15 +60,6 @@ function displayHealth(playerHealth){
     health.appendChild(image);
     health.style.backgroundSize = `${playerHealth}%`
 }
-
-displayHealth(playerHealth);
-
-
-/* Combat Happens and HP gets deducted */
-let soulTap = document.createElement("button");
-soulTap.textContent = "SoulTap";
-soulTap.addEventListener("click", dealDamage);
-document.querySelector(".fight").appendChild(soulTap);
 
 function dealDamage() {
     return damagePlayer(20);
@@ -104,21 +72,76 @@ function damagePlayer(damage) {
 }
 
 function dragstartHandler(e) {
- e.dataTransfer.setData("id", e.target.id);
+    e.dataTransfer.setData("id", e.target.id);
 }
 
 function dragoverHandler(e) {
-  e.preventDefault();
-  if (e.target.getAttribute("draggable") === true) {
-      e.dataTransfer.dropEffect = "none";}
-  else {e.dataTransfer.dropEffect = "all";}
+    e.preventDefault();
+    if (e.target.getAttribute("draggable") === true) {
+        e.dataTransfer.dropEffect = "none";
+    } else {
+        e.dataTransfer.dropEffect = "all";
+    }
 }
 
-function dropHandler (e) {
-  e.preventDefault();
-  let id = e.dataTransfer.getData("id");
-  let dragged = document.getElementById(id);
-  e.target.appendChild(dragged);
-  dragged.setAttribute("draggable", "false");
-  dragged.style.cursor = "not-allowed";
+function dropHandler(e) {
+    e.preventDefault();
+    let id = e.dataTransfer.getData("id");
+    let dragged = document.getElementById(id);
+    e.target.appendChild(dragged);
+    dragged.setAttribute("draggable", "false");
+    dragged.style.cursor = "not-allowed";
 }
+
+function main() {
+    let cards = [
+        {
+            "name": "Nemo",
+            "power": 2,
+            "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+            "health": 6,
+            "mana": 9,
+        },
+        {
+            "name": "Nemo",
+            "power": 2,
+            "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+            "health": 6,
+            "mana": 9,
+        },
+        {
+            "name": "Nemo",
+            "power": 2,
+            "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+            "health": 6,
+            "mana": 9,
+        },
+        {
+            "name": "Nemo",
+            "power": 2,
+            "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+            "health": 6,
+            "mana": 9,
+        },
+    ];
+
+    // TODO randomize cards
+
+    for (let card of cards.slice(0,4)) {
+        createCard(card)
+    }
+
+    let playerHealth = 100;
+
+
+    displayHealth(playerHealth);
+
+
+    /* Combat Happens and HP gets deducted */
+    let soulTap = document.createElement("button");
+    soulTap.textContent = "SoulTap";
+    soulTap.addEventListener("click", dealDamage);
+    document.querySelector(".fight").appendChild(soulTap);
+}
+
+main();
