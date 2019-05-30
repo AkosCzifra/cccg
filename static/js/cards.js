eeebc46032a88e03d73a1ddc966acc512994a855
Jankaral <janoskrizsan402@gmail.com>
2019-05-30 13:21:02 +0200

    merge

--- static/js/cards.js
+++ static/js/cards.js
@@ -28,3 +28,34 @@ let cards = [
         "mana": 9,
     },
 ];
+
+let enemyCards = [
+    {
+        "name": "Enem1",
+        "power": 2,
+        "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+        "health": 6,
+        "mana": 9,
+    },
+    {
+        "name": "Enem2",
+        "power": 2,
+        "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+        "health": 6,
+        "mana": 9,
+    },
+    {
+        "name": "Enem3",
+        "power": 2,
+        "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+        "health": 6,
+        "mana": 9,
+    },
+    {
+        "name": "Enem4",
+        "power": 2,
+        "image": "https://pbs.twimg.com/profile_images/988980087147053058/uUQiBEyG_400x400.jpg",
+        "health": 6,
+        "mana": 9,
+    },
+];
